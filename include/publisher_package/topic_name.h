e38484b0d2402a8cca49fbd835c2bd8201020ea6
tejaswiniR161 <tejaswinigowda1996@gmail.com>
2019-09-12 16:51:43 -0400

    Topic picked up from header file

---
+++ include/publisher_package/topic_name.h
@@ -0,0 +1,6 @@
+#ifndef TOPIC_NAME_H
+#define TOPIC_NAME_H
+#include <string>
+std::string topic_name("talk");
+#endif
+
--- publisher_code.cpp
+++ publisher_code.cpp
@@ -1,4 +1,3 @@
-#include "publisher_package/topic_name.h"
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
@@ -19,7 +18,7 @@ int main(int argc, char **argv)
    * You must call one of the versions of ros::init() before using any other
    * part of the ROS system.
    */
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "publisher_node");
 
   /**
    * NodeHandle is the main access point to communications with the ROS system.
@@ -45,7 +44,7 @@ int main(int argc, char **argv)
    * than we can send them, the number here specifies how many messages to
    * buffer up before throwing some away.
    */
-  ros::Publisher chatter_pub = n.advertise<std_msgs::String>(topic_name, 1000);
+  ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1000);
 
   ros::Rate loop_rate(10);
 
